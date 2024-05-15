3548635fcab8ff68f60d796b19766ee050a194d8
candy-dev <contractdeveloper287@gmail.com>
2024-05-15 23:16:03 +0530

    add test for treasury

--- src/BondingERC20TokenFactory.sol
+++ src/BondingERC20TokenFactory.sol
@@ -12,33 +12,42 @@ event TreasuryUpdated(address indexed newTreasury, address indexed oldTreasury);
 event TokenDeployed(address indexed token);
 
 contract BondingERC20TokenFactory is Ownable {
-    IBondingCurve public bondingCurve;
-    address public treasury;
+  IBondingCurve public bondingCurve;
+  address public treasury;
 
-    constructor(address _owner) Ownable(_owner) { }
+  constructor(address _owner, IBondingCurve _bondingCurve, address _treasury) Ownable(_owner) {
+    bondingCurve = _bondingCurve;
+    treasury = _treasury;
+  }
 
-    function deployBondingERC20Token(
-        address _router,
-        string memory _name,
-        string memory _symbol,
-        uint256 _buyFee,
-        uint256 _sellFee
-    )
-        public
-    {
-        ContinuosBondingERC20Token _bondingERC20Token =
-            new ContinuosBondingERC20Token(_router, _name, _symbol, treasury, _buyFee, _sellFee, bondingCurve);
+  function deployBondingERC20Token(
+    address _router,
+    string memory _name,
+    string memory _symbol,
+    uint256 _buyFee,
+    uint256 _sellFee
+  ) public returns (address) {
+    ContinuosBondingERC20Token _bondingERC20Token = new ContinuosBondingERC20Token(
+      _router,
+      _name,
+      _symbol,
+      treasury,
+      _buyFee,
+      _sellFee,
+      bondingCurve
+    );
+    emit TokenDeployed(address(_bondingERC20Token));
 
-        emit TokenDeployed(address(_bondingERC20Token));
-    }
+    return address(_bondingERC20Token);
+  }
 
-    function updateTreasury(address _newTreasury) public onlyOwner {
-        emit TreasuryUpdated(_newTreasury, treasury);
-        treasury = _newTreasury;
-    }
+  function updateTreasury(address _newTreasury) public onlyOwner {
+    emit TreasuryUpdated(_newTreasury, treasury);
+    treasury = _newTreasury;
+  }
 
-    function updateBondingCurve(IBondingCurve _newBondingCurve) public onlyOwner {
-        emit BondingCurveUpdated(address(_newBondingCurve), address(bondingCurve));
-        bondingCurve = _newBondingCurve;
-    }
+  function updateBondingCurve(IBondingCurve _newBondingCurve) public onlyOwner {
+    emit BondingCurveUpdated(address(_newBondingCurve), address(bondingCurve));
+    bondingCurve = _newBondingCurve;
+  }
 }
---
+++ test/BondingERC20TokenFactory.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { Test } from "forge-std/src/Test.sol";
+import { console2 } from "forge-std/src/console2.sol";
+
+import { BondingERC20TokenFactory, TokenDeployed } from "src/BondingERC20TokenFactory.sol";
+import { IBondingCurve } from "src/interfaces/IBondingCurve.sol";
+import { IContinuousBondingERC20Token } from "src/interfaces/IContinuousBondingERC20Token.sol";
+import { BancorFormula } from "src/BancorCurve/BancorFormula.sol";
+
+contract BondingERC20TokenFactoryTest is Test {
+  BondingERC20TokenFactory internal factory;
+  IBondingCurve internal bondingCurve;
+  address internal router;
+  address internal treasury = makeAddr("treasury");
+  address internal owner = makeAddr("owner");
+
+  function setUp() public {
+    uint256 forkId = vm.createFork(vm.envString("ETH_RPC_URL"), 19876830);
+    vm.selectFork(forkId);
+
+    router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    bondingCurve = new BancorFormula();
+    factory = new BondingERC20TokenFactory(owner, bondingCurve, treasury);
+  }
+
+  function testDeployBondingERC20TokenSuccess() public {
+    address bondingERC20Token = factory.deployBondingERC20Token(router, "ERC20Token", "ERC20", 100, 100);
+
+    assertNotEq(bondingERC20Token, address(0));
+    assertEq(IContinuousBondingERC20Token(bondingERC20Token).bondingCurve(), address(bondingCurve));
+    assertEq(IContinuousBondingERC20Token(bondingERC20Token).TREASURY_ADDRESS(), treasury);
+  }
+
+  function testUpdateBondingCurveSuccess() public {
+    address bondingERC20TokenOldBondingCurve = factory.deployBondingERC20Token(router, "ERC20Token", "ERC20", 100, 100);
+    IBondingCurve newBondingCurve = new BancorFormula();
+
+    vm.startPrank(owner);
+    factory.updateBondingCurve(newBondingCurve);
+
+    address bondingERC20TokenNewBondingCurve = factory.deployBondingERC20Token(router, "ERC20Token", "ERC20", 100, 100);
+
+    assertEq(IContinuousBondingERC20Token(bondingERC20TokenOldBondingCurve).bondingCurve(), address(bondingCurve));
+    assertEq(IContinuousBondingERC20Token(bondingERC20TokenNewBondingCurve).bondingCurve(), address(newBondingCurve));
+  }
+
+  function testUpdateBondingCurveFail() public {
+    IBondingCurve newBondingCurve = new BancorFormula();
+
+    vm.startPrank(makeAddr("random"));
+    vm.expectRevert();
+    factory.updateBondingCurve(newBondingCurve);
+  }
+
+  function testUpdateTreasurySuccess() public {
+    address bondingERC20TokenOldTreasury = factory.deployBondingERC20Token(router, "ERC20Token", "ERC20", 100, 100);
+    address newTreasury = makeAddr("newTreasury");
+
+    vm.startPrank(owner);
+    factory.updateTreasury(newTreasury);
+
+    address bondingERC20TokenNewTreasury = factory.deployBondingERC20Token(router, "ERC20Token", "ERC20", 100, 100);
+
+    assertEq(IContinuousBondingERC20Token(bondingERC20TokenOldTreasury).TREASURY_ADDRESS(), treasury);
+    assertEq(IContinuousBondingERC20Token(bondingERC20TokenNewTreasury).TREASURY_ADDRESS(), newTreasury);
+  }
+
+  function testUpdateTreasuryFail() public {
+    address newTreasury = makeAddr("newTreasury");
+
+    vm.startPrank(makeAddr("random"));
+    vm.expectRevert();
+    factory.updateTreasury(newTreasury);
+  }
+}
