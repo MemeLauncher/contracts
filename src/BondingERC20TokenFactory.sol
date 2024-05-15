aadb25653db262a984489a3585eaf3c301ab72e4
candy-dev <contractdeveloper287@gmail.com>
2024-05-15 19:37:54 +0530

    add factory

---
+++ src/BondingERC20TokenFactory.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ContinuosBondingERC20Token } from "./ContinuosBondingERC20Token.sol";
+import { IBondingCurve } from "./interfaces/IBondingCurve.sol";
+
+event BondingCurveUpdated(address indexed newBondingCurve, address indexed oldBondingCurve);
+
+event TreasuryUpdated(address indexed newTreasury, address indexed oldTreasury);
+
+event TokenDeployed(address indexed token);
+
+contract BondingERC20TokenFactory is Ownable {
+    IBondingCurve public bondingCurve;
+    address public treasury;
+
+    constructor(address _owner) Ownable(_owner) { }
+
+    function deployBondingERC20Token(
+        address _router,
+        string memory _name,
+        string memory _symbol,
+        uint256 _buyFee,
+        uint256 _sellFee
+    )
+        public
+    {
+        ContinuosBondingERC20Token _bondingERC20Token =
+            new ContinuosBondingERC20Token(_router, _name, _symbol, treasury, _buyFee, _sellFee, bondingCurve);
+
+        emit TokenDeployed(address(_bondingERC20Token));
+    }
+
+    function updateTreasury(address _newTreasury) public onlyOwner {
+        emit TreasuryUpdated(_newTreasury, treasury);
+        treasury = _newTreasury;
+    }
+
+    function updateBondingCurve(IBondingCurve _newBondingCurve) public onlyOwner {
+        emit BondingCurveUpdated(address(_newBondingCurve), address(bondingCurve));
+        bondingCurve = _newBondingCurve;
+    }
+}
---
+++ src/interfaces/IBondingCurve.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+interface IBondingCurve {
+    function calculatePurchaseReturn(
+        uint256 _supply,
+        uint256 _connectorBalance,
+        uint32 _connectorWeight,
+        uint256 _depositAmount
+    )
+        external
+        view
+        returns (uint256);
+
+    function calculateSaleReturn(
+        uint256 _supply,
+        uint256 _connectorBalance,
+        uint32 _connectorWeight,
+        uint256 _sellAmount
+    )
+        external
+        view
+        returns (uint256);
+}
